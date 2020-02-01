a4f6b7dbf7bb704c21ba4d65af0d5c2358b2ad2f
Micah Evans <micah.s.evans@gmail.com>
2020-02-01 02:44:07 -0500

    Fixed transitions on page load and toggle effects.

--- index.js
+++ index.js
@@ -2,31 +2,64 @@ $(document).ready(function () {
 
     $(function menuToggle1() {
 
+        $(".aboutMe").click(function () {
+
+            event.preventDefault()
+
+            $(".contentContainer1, .contentContainer2, .contentContainer3").fadeOut('slow', function () {
+
+                $(".contentContainer1").fadeIn('slow');
+
+                // $(".contentContainer1").toggle();
+
+            })
+
+        });
+
+    });
+
+
+    $(function menuToggle2() {
+
         $(".contentContainer2, .contentContainer3").hide();
 
         $(".portfolio").click(function () {
 
             event.preventDefault()
 
+            let portfolioState = ".contentContainer1, .contentContainer3";
+
             $(".contentContainer1, .contentContainer3").hide();
 
-            $(".contentContainer2").toggle();
+            $(portfolioState).fadeOut('slow', function () {
+
+                $(".contentContainer2").fadeIn('slow');
+
+                // $(".contentContainer2").toggle();
+
+            })
 
         });
 
     });
 
-    $(function menuToggle2() {
-
-        // $(".contentContainer2, .contentContainer3").hide();
+    $(function menuToggle3() {
 
         $(".resume").click(function () {
 
             event.preventDefault()
 
+            let resumeState = ".contentContainer1, .contentContainer2";
+
             $(".contentContainer1, .contentContainer2").hide();
 
-            $(".contentContainer3").toggle();
+            $(resumeState).fadeOut('slow', function () {
+
+                $(".contentContainer3").fadeIn('slow');
+
+                // $(".contentContainer3").toggle();
+
+            })
 
         });
 
